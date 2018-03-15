5daca265a1c37474b85bc71abab59759b7395fbf
Sahana Balram <sahana2008balram@gmail.com>
2018-03-15 15:33:13 -0700

    Title component

---
+++ src/components/Title/Title.js
@@ -0,0 +1,14 @@
+import React,{Component} from 'react';
+
+class Titles extends Component{
+    render(){
+        return(
+            <div>
+            <h1>Recipe Box</h1>
+            <h3>Checkout your favourite recipes here and do changes to them as you please...</h3>
+            </div>
+        )
+    }
+}
+
+export default Titles;

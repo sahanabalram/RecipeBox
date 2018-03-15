0785813bae64c16b1ed6271f695f097602c4df51
Sahana Balram <sahana2008balram@gmail.com>
2018-03-15 15:50:11 -0700

    Import Cards CSS file

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -1,13 +1,13 @@
 import React, {Component} from 'react';
-
+import './Cards.css';
 class RecipeCards extends Component {
     render() {
         return (
-            <div class="card border-info mb-3">
-                <div class="card-header">Header</div>
-                <div class="card-body">
-                    <h4 class="card-title">Info card title</h4>
-                    <p class="card-text">Some quick example text to build on the card title and make
+            <div className="card border-info mb-3">
+                <div className="card-header">Header</div>
+                <div className="card-body">
+                    <h4 className="card-title">Info card title</h4>
+                    <p className="card-text">Some quick example text to build on the card title and make
                         up the bulk of the card's content.</p>
                 </div>
             </div>

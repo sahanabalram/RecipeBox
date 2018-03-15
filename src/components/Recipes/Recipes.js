672922284ae1243e59e41b15964ad70ba7ec6db9
Sahana Balram <sahana2008balram@gmail.com>
2018-03-15 15:36:16 -0700

    Main recipes component

---
+++ src/components/Recipes/Recipes.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class Recipes extends Component {
+    render(){
+        return(
+            <div>Recipe Component</div>
+        )
+    }
+}
+
+export default Recipes;

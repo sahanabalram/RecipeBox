c8f21083471d3cc1d07523cc9de5a9c4ffe68bb4
Sahana Balram <sahana2008balram@gmail.com>
2018-03-15 20:35:02 -0700

    Create Delete, Edit and Add button

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -5,15 +5,33 @@ import {
     Col,
     Panel,
     PanelGroup,
+    ButtonToolbar,
+    Button,
     ListGroup,
-    ListGroupItem
+    ListGroupItem,
+    Modal
 } from 'react-bootstrap';
 import './Cards.css';
 
 class RecipePanel extends Component {
-    state = {
-        recipes: this.props.recipes || []
-    };
+    constructor(props, context) {
+        super(props, context);
+    
+        this.handleShow = this.handleShow.bind(this);
+        this.handleClose = this.handleClose.bind(this);
+    
+        this.state = {
+          show: false,
+          recipes: this.props.recipes || []
+        };
+      }
+      handleClose() {
+        this.setState({ show: false });
+      }
+    
+      handleShow() {
+        this.setState({ show: true });
+      }
     render() {
         return (
             <div>
@@ -23,7 +41,7 @@ class RecipePanel extends Component {
                             .state
                             .recipes
                             .map(recipeList => (
-                                <Col s={12} m={4} lg={4}>
+                                <Col s={12} m={12} lg={12}>
                                     <PanelGroup accordion id="accordion-uncontrolled-example" defaultActiveKey="2">
                                         <Panel eventKey="1">
                                             <Panel.Heading>
@@ -37,6 +55,10 @@ class RecipePanel extends Component {
                                                             <ListGroupItem>{ingredient}</ListGroupItem>
                                                         ))}
                                                 </ListGroup>
+                                                        <ButtonToolbar>
+                                                            <Button bsStyle="danger">Delete</Button>
+                                                            <Button bsStyle="default">Edit</Button>
+                                                        </ButtonToolbar>
                                             </Panel.Body>
                                         </Panel>
                                     </PanelGroup>
@@ -44,6 +66,18 @@ class RecipePanel extends Component {
                             ))}
 
                     </Row>
+                    <ButtonToolbar>
+                        <Button bsStyle="info">Add Recipes</Button>
+                    </ButtonToolbar>
+
+                    {/*Modal for add recipes code */}
+                    <Modal show={this.state.show} onHide={this.handleClose}>
+                    <Modal.Header closeButton>
+                      <Modal.Title>Modal heading</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                    </Modal.Body>
+                    </Modal>
                 </Grid>
             </div>
 

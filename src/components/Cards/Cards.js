b6398aac467f160a5e22162e9d01cf15c7484365
Sahana Balram <sahana2008balram@gmail.com>
2018-03-15 18:22:08 -0700

    Rewrite the recipe component

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -1,36 +1,62 @@
 import React, {Component} from 'react';
-import {Grid, Row, Col} from 'react-bootstrap';
+import {Panel,PanelGroup} from 'react-bootstrap';
 import './Cards.css';
-class RecipeCards extends Component {
+
+class RecipePanel extends Component {
     state = {
         recipes: this.props.recipes || []
     };
     render() {
         return (
-            <div className="container">
-                <Grid>
-                    <Row>
-                        {this
-                            .state
-                            .recipes
-                            .map(recipeList => (
-                                <Col s={12} m={4} lg={4}>
-                                    <div className="card border-info mb-3">
-                                        <div className="card-header">{recipeList.name}</div>
-                                        <div className="card-body">
-                                            <p className="card-text">{recipeList.ingredients}</p>
-                                        </div>
-                                    </div>
-                                </Col>
-                            ))}
-
-                    </Row>
-                </Grid>
-
-            </div>
-
+            <PanelGroup accordion id="accordion-example">
+                <Panel eventKey="1">
+                    <Panel.Heading>
+                        <Panel.Title toggle>Collapsible Group Item #1</Panel.Title>
+                    </Panel.Heading>
+                    <Panel.Body collapsible>
+                        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry
+                        richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor
+                        brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor,
+                        sunt aliqua put a bird on it squid single-origin coffee nulla assumenda
+                        shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson
+                        cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo.
+                        Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt
+                        you probably haven't heard of them accusamus labore sustainable VHS.
+                    </Panel.Body>
+                </Panel>
+                <Panel eventKey="2">
+                    <Panel.Heading>
+                        <Panel.Title toggle>Collapsible Group Item #2</Panel.Title>
+                    </Panel.Heading>
+                    <Panel.Body collapsible>
+                        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry
+                        richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor
+                        brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor,
+                        sunt aliqua put a bird on it squid single-origin coffee nulla assumenda
+                        shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson
+                        cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo.
+                        Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt
+                        you probably haven't heard of them accusamus labore sustainable VHS.
+                    </Panel.Body>
+                </Panel>
+                <Panel eventKey="3">
+                    <Panel.Heading>
+                        <Panel.Title toggle>Collapsible Group Item #3</Panel.Title>
+                    </Panel.Heading>
+                    <Panel.Body collapsible>
+                        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry
+                        richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor
+                        brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor,
+                        sunt aliqua put a bird on it squid single-origin coffee nulla assumenda
+                        shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson
+                        cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo.
+                        Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt
+                        you probably haven't heard of them accusamus labore sustainable VHS.
+                    </Panel.Body>
+                </Panel>
+            </PanelGroup>
         )
     }
 }
 
-export default RecipeCards;
+export default RecipePanel;

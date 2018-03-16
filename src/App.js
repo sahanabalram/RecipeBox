611b5d45e3c5f88afcf2a06b617583196d3ac8e9
Sahana Balram <sahana2008balram@gmail.com>
2018-03-15 22:21:42 -0700

    Remove recipe component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import React, { Component } from 'react';
+import './App.css';
 import Titles from './components/Title';
-// import Recipes from './components/Recipes'; 
 import RecipePanel from './components/Cards';
 import RecipeDetails from './Recipe.json';
-import './App.css';
-
 class App extends Component {
   render() {
     return (
       <div className="App">
       <Titles/>
-    {/*<Recipes/>*/}
       <RecipePanel recipes={RecipeDetails.recipes}/>
       </div>
     );

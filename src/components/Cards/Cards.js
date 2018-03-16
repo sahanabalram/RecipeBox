39b06cb242db0d1f593afb0bf37dcd6f01c437b8
Sahana Balram <sahana2008balram@gmail.com>
2018-03-15 20:12:26 -0700

    Create Panel

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -1,5 +1,13 @@
 import React, {Component} from 'react';
-import {Panel,PanelGroup} from 'react-bootstrap';
+import {
+    Grid,
+    Row,
+    Col,
+    Panel,
+    PanelGroup,
+    ListGroup,
+    ListGroupItem
+} from 'react-bootstrap';
 import './Cards.css';
 
 class RecipePanel extends Component {
@@ -8,53 +16,37 @@ class RecipePanel extends Component {
     };
     render() {
         return (
-            <PanelGroup accordion id="accordion-example">
-                <Panel eventKey="1">
-                    <Panel.Heading>
-                        <Panel.Title toggle>Collapsible Group Item #1</Panel.Title>
-                    </Panel.Heading>
-                    <Panel.Body collapsible>
-                        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry
-                        richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor
-                        brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor,
-                        sunt aliqua put a bird on it squid single-origin coffee nulla assumenda
-                        shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson
-                        cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo.
-                        Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt
-                        you probably haven't heard of them accusamus labore sustainable VHS.
-                    </Panel.Body>
-                </Panel>
-                <Panel eventKey="2">
-                    <Panel.Heading>
-                        <Panel.Title toggle>Collapsible Group Item #2</Panel.Title>
-                    </Panel.Heading>
-                    <Panel.Body collapsible>
-                        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry
-                        richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor
-                        brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor,
-                        sunt aliqua put a bird on it squid single-origin coffee nulla assumenda
-                        shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson
-                        cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo.
-                        Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt
-                        you probably haven't heard of them accusamus labore sustainable VHS.
-                    </Panel.Body>
-                </Panel>
-                <Panel eventKey="3">
-                    <Panel.Heading>
-                        <Panel.Title toggle>Collapsible Group Item #3</Panel.Title>
-                    </Panel.Heading>
-                    <Panel.Body collapsible>
-                        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry
-                        richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor
-                        brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor,
-                        sunt aliqua put a bird on it squid single-origin coffee nulla assumenda
-                        shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson
-                        cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo.
-                        Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt
-                        you probably haven't heard of them accusamus labore sustainable VHS.
-                    </Panel.Body>
-                </Panel>
-            </PanelGroup>
+            <div>
+                <Grid>
+                    <Row>
+                        {this
+                            .state
+                            .recipes
+                            .map(recipeList => (
+                                <Col s={12} m={4} lg={4}>
+                                    <PanelGroup accordion id="accordion-uncontrolled-example" defaultActiveKey="2">
+                                        <Panel eventKey="1">
+                                            <Panel.Heading>
+                                                <Panel.Title toggle>{recipeList.name}</Panel.Title>
+                                            </Panel.Heading>
+                                            <Panel.Body collapsible>
+                                                <ListGroup>
+                                                    {recipeList
+                                                        .ingredients
+                                                        .map(ingredient => (
+                                                            <ListGroupItem>{ingredient}</ListGroupItem>
+                                                        ))}
+                                                </ListGroup>
+                                            </Panel.Body>
+                                        </Panel>
+                                    </PanelGroup>
+                                </Col>
+                            ))}
+
+                    </Row>
+                </Grid>
+            </div>
+
         )
     }
 }

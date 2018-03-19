2c020c75b974ad9ec3499f779abe5eb52c269248
Sahana Balram <sahana2008balram@gmail.com>
2018-03-19 14:25:14 -0700

    Add image and import CSS file

--- src/components/Title/Title.js
+++ src/components/Title/Title.js
@@ -1,10 +1,10 @@
 import React,{Component} from 'react';
-
+import "./Title.css";
 class Titles extends Component{
     render(){
         return(
             <div>
-            <h1>Recipe Box</h1>
+            <img src="http://netnebraska.org/sites/default/files/styles/basic-page-feature-image-standalone/public/page/recipebox_featureimage.jpg?itok=7tWbC8_I" alt="Recipe Box"/>
             <h3>Checkout your favourite recipes here and do changes to them as you please...</h3>
             </div>
         )
